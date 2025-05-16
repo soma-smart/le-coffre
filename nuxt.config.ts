a00606d6dc6248c769f0ad9ef632e00ff62e5b04
Fabien <fgouget@soma-smart.com>
2025-05-16 12:42:14 +0000

    copy db migration on close build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,7 @@
+import { cpSync } from 'node:fs'
+import { resolve } from 'node:path'
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -13,4 +17,18 @@ export default defineNuxtConfig({
     },
   },
   devtools: { enabled: true },
+  hooks: {
+    close: () => {
+      const migrationsSrc = resolve('server/database/migrations')
+      const migrationsDest = resolve('.output/server/database/migrations')
+      try {
+        // Copy the migrations directory recursively
+        cpSync(migrationsSrc, migrationsDest, { recursive: true })
+        console.log('Database migration files have been copied to the static directory.')
+      }
+      catch (error) {
+        console.error('Failed to copy migration files:', error)
+      }
+    },
+  },
 })
