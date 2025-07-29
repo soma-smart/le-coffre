af4e1c112e390de80eefb238a52a31f77a33fedf
Vianney Bacoup <vbacoup@soma-smart.com>
2025-07-29 21:26:02 +0200

    Add main that runs everything

---
+++ server/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from src.vault_management_context.adapters.secondary.gateways.fake_vault_repository import (
+    FakeVaultRepository,
+)
+from src.vault_management_context.adapters.secondary.gateways.crypto_shamir_gateway import (
+    CryptoShamirGateway,
+)
+
+from src.vault_management_context.adapters.primary.api.routes import (
+    vault_management_route,
+)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    vault_repository = FakeVaultRepository()
+    shamir_gateway = CryptoShamirGateway()
+
+    app.state.vault_repository = vault_repository
+    app.state.shamir_gateway = shamir_gateway
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(vault_management_route.router)
