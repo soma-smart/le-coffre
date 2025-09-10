a0c6ef20699944ee2d0af15d441fbca7a2d15c1c
Quentin <qlobet@soma-smart.com>
2025-09-10 12:35:42 +0000

    feat: implement password complexity validation in CreatePasswordUseCase and add related exceptions

--- server/src/password_management_context/application/use_cases/create_password_use_case.py
+++ server/src/password_management_context/application/use_cases/create_password_use_case.py
@@ -3,6 +3,10 @@ from uuid import UUID
 from password_management_context.application.commands import CreatePasswordCommand
 from password_management_context.application.gateways import PasswordRepository
 from password_management_context.domain.entities import Password
+from password_management_context.domain.services.password_complexity_service import (
+    PasswordComplexityService,
+)
+from password_management_context.domain.exceptions import PasswordComplexityError
 from shared_kernel.encryption import EncryptionService
 from shared_kernel.access_control import AccessController
 
@@ -19,6 +23,10 @@ class CreatePasswordUseCase:
         self.access_controller = access_controller
 
     def execute(self, command: CreatePasswordCommand) -> UUID:
+        violations = PasswordComplexityService.validate(command.decrypted_password)
+        if violations:
+            raise PasswordComplexityError(violations)
+
         encrypted_value = self.encryption_service.encrypt(command.decrypted_password)
 
         password = Password.create(
