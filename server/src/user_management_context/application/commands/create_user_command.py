8b08d82100bf172eb9e10716fa80db12776e3216
Fabien <fgouget@soma-smart.com>
2025-09-15 07:30:02 +0000

    refactor: remove unused Optional import and static method from CreateUserCommand; delete resource_id method from UserRepository

--- server/src/user_management_context/application/commands/create_user_command.py
+++ server/src/user_management_context/application/commands/create_user_command.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from uuid import UUID, uuid4
-from typing import Optional
 
 
 @dataclass
@@ -10,6 +9,7 @@ class CreateUserCommand:
     email: str
     password: str
 
+    @staticmethod
     def from_dict(data: dict) -> "CreateUserCommand":
         return CreateUserCommand(
             id=UUID(data["id"]) if "id" in data else uuid4(),
