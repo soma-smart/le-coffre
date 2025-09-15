5e6170d82a0467017926285390e26d054a153fe5
Fabien <fgouget@soma-smart.com>
2025-09-15 07:36:43 +0000

    refactor: update UserNotFoundError to accept both UUID and email as parameters

--- server/src/user_management_context/domain/exceptions.py
+++ server/src/user_management_context/domain/exceptions.py
@@ -9,8 +9,8 @@ class UserManagementDomainError(Exception):
 class UserNotFoundError(UserManagementDomainError):
     """Raised when attempting to get a password not existing"""
 
-    def __init__(self, uuid: UUID):
-        super().__init__(f"The requested user with ID {uuid} was not found")
+    def __init__(self, uunid_or_email: str | UUID):
+        super().__init__(f"The requested user with {uunid_or_email} was not found")
 
 
 class UserAlreadyExistsError(UserManagementDomainError):
