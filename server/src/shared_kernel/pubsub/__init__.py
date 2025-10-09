53e294d825a70f46c7226b8c221ad6baa1b65677
Vianney Bacoup <vbacoup@soma-smart.com>
2025-10-09 10:30:42 +0200

    feat: update __all__ exports in pubsub module

--- server/src/shared_kernel/pubsub/__init__.py
+++ server/src/shared_kernel/pubsub/__init__.py
@@ -3,3 +3,9 @@ from shared_kernel.pubsub.adapters.in_memory_event_publisher import (
     InMemoryDomainEventPublisher,
 )
 from shared_kernel.pubsub.gateway.event_publisher_gateway import DomainEventPublisher
+
+__all__ = [
+    "DomainEvent",
+    "InMemoryDomainEventPublisher",
+    "DomainEventPublisher",
+]
