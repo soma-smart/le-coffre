a10c25d4452608b53ff07a1ac1c8f8113849baad
Vianney Bacoup <vbacoup@soma-smart.com>
2026-01-08 17:34:06 +0000

    update link to backend with localhost to build for api openapi.json

--- frontend/openapi-ts.config.ts
+++ frontend/openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-  input: 'http://backend:8000/api/openapi.json',
+  input: 'http://localhost:8000/api/openapi.json',
   output: 'src/client',
   baseUrl: '/',
 });
