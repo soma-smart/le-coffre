df86724dc88c3a3c3a69a22174b754c6228a5626
Vianney Bacoup <vbacoup@soma-smart.com>
2026-01-24 18:06:20 +0000

    update bcryp to handle password  > 72 bytes

--- server/src/identity_access_management_context/adapters/secondary/bcrypt_hashing_gateway.py
+++ server/src/identity_access_management_context/adapters/secondary/bcrypt_hashing_gateway.py
@@ -2,11 +2,16 @@ from identity_access_management_context.application.gateways import (
     PasswordHashingGateway,
 )
 import bcrypt
+import hashlib
 
 
 class BcryptHashingGateway(PasswordHashingGateway):
     def hash(self, password: str) -> bytes:
-        return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        return bcrypt.hashpw(
+            hashlib.sha256(password.encode()).digest(), bcrypt.gensalt()
+        )
 
     def verify(self, password: str, hashed_password: bytes) -> bool:
-        return bcrypt.checkpw(password.encode(), hashed_password)
+        return bcrypt.checkpw(
+            hashlib.sha256(password.encode()).digest(), hashed_password
+        )
