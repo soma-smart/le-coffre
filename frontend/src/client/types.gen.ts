c6e824b0f2a5c3bd7ab497fe81e45b62ed7a761c
Vianney Bacoup <vbacoup@soma-smart.com>
2026-02-25 10:02:16 +0000

    Regenerate api in frontend

--- frontend/src/client/types.gen.ts
+++ frontend/src/client/types.gen.ts
@@ -866,6 +866,16 @@ export type ValidationError = {
      * Error Type
      */
     type: string;
+    /**
+     * Input
+     */
+    input?: unknown;
+    /**
+     * Context
+     */
+    ctx?: {
+        [key: string]: unknown;
+    };
 };
 
 /**
