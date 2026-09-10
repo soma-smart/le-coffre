f8f7129113a9983f925f965f22274dc2f6d270bb
Gabriel Rozzonelli <grozzonelli@soma-smart.com>
2026-09-10 14:24:33 +0000

    feat: add FastAPI driver adapter

--- server/src/identity_access_management_context/adapters/primary/fastapi/routes/group/__init__.py
+++ server/src/identity_access_management_context/adapters/primary/fastapi/routes/group/__init__.py
@@ -3,6 +3,7 @@ from .group_add_member_routes import router as group_add_member_router
 from .group_add_owner_routes import router as group_add_owner_router
 from .group_create_routes import router as group_create_router
 from .group_delete_routes import router as group_delete_router
+from .group_events_list_routes import router as group_events_list_router
 from .group_remove_member_routes import router as group_remove_member_router
 from .group_update_routes import router as group_update_router
 from .list_groups_routes import router as list_groups_router
@@ -16,4 +17,5 @@ __all__ = [
     "get_group_router",
     "list_groups_router",
     "group_delete_router",
+    "group_events_list_router",
 ]
