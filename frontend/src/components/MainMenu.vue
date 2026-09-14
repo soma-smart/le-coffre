b09f84cd1748a7cb876d405411730f67732ae2ac
Gabriel Rozzonelli <grozzonelli@soma-smart.com>
2026-09-14 08:35:12 +0000

    fix(frontend): remove funny easter egg

--- frontend/src/components/MainMenu.vue
+++ frontend/src/components/MainMenu.vue
@@ -112,9 +112,6 @@
         <span class="ml-2 transition-colors" :class="{ 'font-semibold': isProfileActive }"
           >Profile</span
         >
-        <span class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1"
-          >⌘+W</span
-        >
       </div>
       <div v-if="isAdmin">
         <div
